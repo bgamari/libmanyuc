46fef5c521c957e9169ffdbd2eb27a255332f778
Ben Gamari <bgamari.foss@gmail.com>
2012-09-15 17:45:07 -0400

    stm32: Fix startup_stm32l1xx

--- src/ports/stm32/startup_stm32l1xx_md.s
+++ src/ports/stm32/startup_stm32l1xx_md.s
@@ -41,18 +41,19 @@
 
 .global g_pfnVectors
 .global Default_Handler
+.global _startup
 
 /* start address for the initialization values of the .data section. 
 defined in linker script */
-.word _sidata
+.word _start_datai
 /* start address for the .data section. defined in linker script */  
-.word _sdata
+.word _start_data
 /* end address for the .data section. defined in linker script */
-.word _edata
+.word _end_data
 /* start address for the .bss section. defined in linker script */
-.word _sbss
+.word _start_bss
 /* end address for the .bss section. defined in linker script */
-.word _ebss
+.word _end_bss
 
 .equ  BootRAM, 0xF108F85F
 /**
@@ -68,24 +69,25 @@ defined in linker script */
   .weak Reset_Handler
   .type Reset_Handler, %function
 Reset_Handler:
+_startup:       
 
 /* Copy the data segment initializers from flash to SRAM */  
   movs r1, #0
   b LoopCopyDataInit
 
 CopyDataInit:
-  ldr r3, =_sidata
+  ldr r3, =_start_datai
   ldr r3, [r3, r1]
   str r3, [r0, r1]
   adds r1, r1, #4
     
 LoopCopyDataInit:
-  ldr r0, =_sdata
-  ldr r3, =_edata
+  ldr r0, =_start_data
+  ldr r3, =_end_data
   adds r2, r0, r1
   cmp r2, r3
   bcc CopyDataInit
-  ldr r2, =_sbss
+  ldr r2, =_start_bss
   b LoopFillZerobss
 /* Zero fill the bss segment. */  
 FillZerobss:
@@ -93,11 +95,11 @@ FillZerobss:
   str r3, [r2], #4
     
 LoopFillZerobss:
-  ldr r3, = _ebss
+  ldr r3, = _end_bss
   cmp r2, r3
   bcc FillZerobss
 /* Call the clock system intitialization function.*/
-  bl  SystemInit
+  bl  init
 /* Call the application's entry point.*/
   bl main
   bx lr
@@ -128,7 +130,7 @@ Infinite_Loop:
     
     
 g_pfnVectors:
-  .word _estack
+  .word _end_stack
   .word Reset_Handler
   .word NMI_Handler
   .word HardFault_Handler
